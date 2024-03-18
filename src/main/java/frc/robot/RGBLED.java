2fdfcc82ae45d99a5b018b2b182e095a4c68d3aa
Carter Koch <carter7135@gmail.com>
2024-03-18 19:20:23 -0400

    leds

---
+++ src/main/java/frc/robot/RGBLED.java
@@ -0,0 +1,51 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class RGBLED {
+    AddressableLED m_led;
+    AddressableLEDBuffer m_ledBuffer;
+
+    RGBLED(int PWMport){
+        // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(PWMport);
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+    }
+
+    void setColor(int r, int g, int b){
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            // Sets the specified LED to the RGB values for red
+            m_ledBuffer.setRGB(i, r, g, b);
+         }
+         
+         m_led.setData(m_ledBuffer);
+    }
+
+    void rainbow() {
+        int m_rainbowFirstPixelHue = 0;
+        // For every pixel
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+          // Calculate the hue - hue is easier for rainbows because the color
+          // shape is a circle so only one value needs to precess
+          final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+          // Set the value
+          m_ledBuffer.setHSV(i, hue, 255, 128);
+        }
+    
+        // Increase by to make the rainbow "move"
+        m_rainbowFirstPixelHue += 3;
+        // Check bounds
+        m_rainbowFirstPixelHue %= 180;
+    
+      }
+
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,9 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
@@ -33,54 +36,62 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
 
-   SwerveDrive driveTrain;
-   Dumper dumper = new Dumper(29, 6);
-   Climber climber = new Climber(4, 33);
-   Intake intake = new Intake(20,32);
-   Shooter shooter = new Shooter(48, 49);
-   TalonFX falconShooterLeftLeader, falconShooterRightFollower;
-   DriverController driverController;
-   SwerveModule testSwerveModule; //this is a test module
-   Encoder testEncoder = new Encoder(5);
-   XboxController opController = new XboxController(1);
+  SwerveDrive driveTrain;
+  Dumper dumper = new Dumper(29, 6);
+  Climber climber = new Climber(4, 33);
+  Intake intake = new Intake(20, 32);
+  Shooter shooter = new Shooter(48, 49);
+  TalonFX falconShooterLeftLeader, falconShooterRightFollower;
+  DriverController driverController;
+  SwerveModule testSwerveModule; // this is a test module
+  Encoder testEncoder = new Encoder(5);
+  XboxController opController = new XboxController(1);
 
-   SendableChooser<String> autonChooser = new SendableChooser<>();
-   String autonSelected;
-   Timer timer = new Timer();
-   Auton auton;
-   Double time;
+  SendableChooser<String> autonChooser = new SendableChooser<>();
+  String autonSelected;
+  Timer timer = new Timer();
+  Auton auton;
+  Double time;
 
-   double autoGyroOffset = 0.0;
+  String color = "Red";
 
-    //Autons
-    private static final String auDefaultAuton = "Default_Auton";
-    private static final String auAmp_2P = "Amp_2_Piece";
-    private static final String auSource_2P = "Source_2_Piece";
-    private static final String auShootMove = "Shoot_Move";
-    private static final String auCenter_2P = "Center_2_Piece";
-    private static final String auCenter_3P = "Center_3_Piece";
+  double autoGyroOffset = 0.0;
 
-    ShuffleboardTab driveTab = Shuffleboard.getTab("Drive3");
-    GenericEntry intakePieceField = driveTab.add("Have Piece", false).getEntry();
-    GenericEntry gyroAngleField = driveTab.add("Gyro Angle", 0.0).getEntry();
+  RGBLED lights;
 
-    /* Start at velocity 0, enable FOC, no feed forward, use slot 0 */
+  // Autons
+  private static final String auDefaultAuton = "Default_Auton";
+  private static final String auAmp_2P = "Amp_2_Piece";
+  private static final String auSource_2P = "Source_2_Piece";
+  private static final String auShootMove = "Shoot_Move";
+  private static final String auCenter_2P = "Center_2_Piece";
+  private static final String auCenter_3P = "Center_3_Piece";
+
+  ShuffleboardTab driveTab = Shuffleboard.getTab("Drive3");
+  GenericEntry intakePieceField = driveTab.add("Have Piece", false).getEntry();
+  GenericEntry gyroAngleField = driveTab.add("Gyro Angle", 0.0).getEntry();
+
+  /* Start at velocity 0, enable FOC, no feed forward, use slot 0 */
 
   @Override
   public void robotInit() {
     driveTrain = new SwerveDrive();
-    //testSwerveModule = new SwerveModule(5, 14, 0);
+    // testSwerveModule = new SwerveModule(5, 14, 0);
     driverController = new DriverController(driveTrain);
     // driveTrain.driveSet(0, 0.5,0.5, 0.5);
     CameraServer.startAutomaticCapture(0);
@@ -93,12 +104,15 @@ public class Robot extends TimedRobot {
     autonChooser.addOption("Center 3 Piece", auSource_2P);
 
     Shuffleboard.getTab("Drive3").add(autonChooser);
-    
+
+   
+
   }
 
   @Override
   public void robotPeriodic() {
-    if(autonChooser.getSelected() == null || autonChooser.getSelected() == "" || Shuffleboard.getTab("Drive3") == null){
+    if (autonChooser.getSelected() == null || autonChooser.getSelected() == ""
+        || Shuffleboard.getTab("Drive3") == null) {
       System.out.println("Auton Selector Null");
     }
   }
@@ -111,194 +125,213 @@ public class Robot extends TimedRobot {
     autonSelected = autonChooser.getSelected();
     System.out.println(autonSelected);
 
-    switch(autonSelected){
+    switch (autonSelected) {
       case auAmp_2P:
-      auton = new Auton_AmpSide_4P(driveTrain, shooter, intake, false);
-      System.out.println("Running Amp Auto");
-      autoGyroOffset = -54.6; 
-      break;
+        auton = new Auton_AmpSide_4P(driveTrain, shooter, intake, false);
+        System.out.println("Running Amp Auto");
+        autoGyroOffset = -54.6;
+        break;
 
       case auSource_2P:
-      auton = new Auton_SourceSide_2P(driveTrain, shooter, intake, false);
-      autoGyroOffset = -54.6; 
-      System.out.println("Running Source Auto");
-      break;
+        auton = new Auton_SourceSide_2P(driveTrain, shooter, intake, false);
+        autoGyroOffset = -54.6;
+        System.out.println("Running Source Auto");
+        break;
 
       case auCenter_2P:
-      auton = new Auton_Center_2P(driveTrain, shooter, intake, false);
-      System.out.println("Running Center 2 Auto");
-      autoGyroOffset = 0.0;
-      break;
+        auton = new Auton_Center_2P(driveTrain, shooter, intake, false);
+        System.out.println("Running Center 2 Auto");
+        autoGyroOffset = 0.0;
+        break;
 
       case auCenter_3P:
-      //auton = new Auton_Center_3P(driveTrain, shooter, intake, false);
-      System.out.println("Running Center 3 Auto");
-      autoGyroOffset = 0.0;
-      break;
+        // auton = new Auton_Center_3P(driveTrain, shooter, intake, false);
+        System.out.println("Running Center 3 Auto");
+        autoGyroOffset = 0.0;
+        break;
 
       case auShootMove:
-      auton = new Auton_Move(driveTrain, shooter, intake, false);
-      autoGyroOffset = 0.0;
-      break;
-    default:
-      System.out.println("Auton Failed, Default Auto");
-      auton = new Auton_Move(driveTrain, shooter, intake, false);
-      System.out.println("Running No Auto");
-      break;
+        auton = new Auton_Move(driveTrain, shooter, intake, false);
+        autoGyroOffset = 0.0;
+        break;
+      default:
+        System.out.println("Auton Failed, Default Auto");
+        auton = new Auton_Move(driveTrain, shooter, intake, false);
+        System.out.println("Running No Auto");
+        break;
     }
 
-    //This is so we can start against the speaker and still have 0 be away from driver station. Don't need a offset for center spot. 
-    //TODO check if the gyro is good after auton
-    if(autoGyroOffset != 0.0){
+    // This is so we can start against the speaker and still have 0 be away from
+    // driver station. Don't need a offset for center spot.
+    // TODO check if the gyro is good after auton
+    if (autoGyroOffset != 0.0) {
       driveTrain.resetGyroWithOffset(autoGyroOffset);
-    }else{
+    } else {
       driveTrain.resetGyro();
     }
-    
+
   }
 
   @Override
   public void autonomousPeriodic() {
     time = timer.get();
-    if(auton != null){
+    if (auton != null) {
       auton.run(time);
     }
   }
 
   @Override
-  public void teleopInit() { }
+  public void teleopInit() {
+  }
 
   @Override
   public void teleopPeriodic() {
-    //driveTrain.driveSet(0, -1, 0, 0.3);
-    //frontRight.setDrive(0.25);
+    // driveTrain.driveSet(0, -1, 0, 0.3);
+    // frontRight.setDrive(0.25);
 
     driverController.run();
     RelativeEncoder relativeEncoder = driveTrain.backLeft.driveMotorCont.getEncoder();
     System.out.println(relativeEncoder.getVelocity());
 
-    //Stuff to test if swerve is going max speed
+    // Stuff to test if swerve is going max speed
     System.out.println(intake.hasPieceFar());
 
-    //testSwerveModule.setModulePower(1, 0);
-    //System.out.println(testEncoder.getDoubleValue());
+    // testSwerveModule.setModulePower(1, 0);
+    // System.out.println(testEncoder.getDoubleValue());
 
-    /* Operator Controls 
+    /*
+     * Operator Controls
      * A = Shoot, B = Bloop onto Amp
      * LT = Intake, RT = Outtake, RB = Intake until Piece
      * X and Y are Unused, going to be for Amp
      * D Pad for Climber, LB for modified climber
-     * Sticks are Unused 
-    */
+     * Sticks are Unused
+     */
 
-    //Shooter Controls
-    if(opController.getAButton())
-    {
+    // Shooter Controls
+    if (opController.getAButton()) {
       shooter.setTargetRPS(90);
 
-    }else if(opController.getBButton()){
-      //The velocity is in rotations per second
-      shooter.setTargetRPS(3.5); //3.5
+    } else if (opController.getBButton()) {
+      // The velocity is in rotations per second
+      shooter.setTargetRPS(3.5); // 3.5
       intake.setPower(0.8);
-    }else if(opController.getRightTriggerAxis() > 0.3){
+    } else if (opController.getRightTriggerAxis() > 0.3) {
       shooter.setTargetRPS(90);
-      if(shooter.getRPS() > 88){
+      if (shooter.getRPS() > 88) {
         intake.setPower(0.9);
       }
-    }else{
+    } else {
       shooter.setPower(0.0);
     }
 
-    //Intake Controls
-    if(opController.getLeftTriggerAxis() > 0.3){ //Intake
+    // Intake Controls
+    if (opController.getLeftTriggerAxis() > 0.3) { // Intake
       intake.setPower(0.9);
-    }else if(opController.getRightBumper()){ //Outtake
+    } else if (opController.getRightBumper()) { // Outtake
       intake.setPower(-0.2);
-    }else if(opController.getLeftBumper()){ //Intake until piece
+    } else if (opController.getLeftBumper()) { // Intake until piece
       intake.setPowerUntilPiece(0.9);
-    }else if(!opController.getBButton() && !(opController.getRightTriggerAxis() > 0.3)){ //Stop Motor if no controls and if the shooter is not asking for intake controls
+    } else if (!opController.getBButton() && !(opController.getRightTriggerAxis() > 0.3)) { // Stop Motor if no controls and if the shooter is not asking for intake controls
       intake.setPower(0.0);
     }
-    
- 
-    //Climber Controls
-    if(opController.getPOV() == 180){ //Climb Up
+
+    // Climber Controls
+    if (opController.getPOV() == 180) { // Climb Up
       climber.setPowerTogether(-0.6);
-    }else if(opController.getPOV() == 0){ //Climb Down
+    } else if (opController.getPOV() == 0) { // Climb Down
       climber.setPowerTogether(0.6);
-    }else if(opController.getLeftY() <  -0.3){ //Climb Down Left
+    } else if (opController.getLeftY() < -0.3) { // Climb Down Left
       climber.setLeftPower(-0.3);
-    }else if(opController.getRightY() < -0.3){ //Climb Down Right
+    } else if (opController.getRightY() < -0.3) { // Climb Down Right
       climber.setRightPower(-0.3);
-    }else if(opController.getLeftY() > 0.3){ //Climb Up Left
+    } else if (opController.getLeftY() > 0.3) { // Climb Up Left
       climber.setLeftPower(0.3);
-    }else if(opController.getRightY() > 0.3){ //Climb Up Right
+    } else if (opController.getRightY() > 0.3) { // Climb Up Right
       climber.setRightPower(0.3);
-    }else{ //Turn off power
+    } else { // Turn off power
       climber.setPowerTogether(0);
     }
 
-
-    //Puts a bool to the shuffleboard saying if we have a piece.
+    // Puts a bool to the shuffleboard saying if we have a piece.
     intakePieceField.setBoolean(!intake.hasPiece());
     gyroAngleField.setDouble(driveTrain.gyro.getAngle());
 
+    // 50 ticks per sec
+    // one button flip
+    if (opController.getYButtonPressed()) {
 
-    //50 ticks per sec
-    //one button flip
-    if(opController.getYButtonPressed()){
-      
     }
-    /*  for (int i = 0; i < 152; i++) {
-        if(i < 70){//bloop
-          shooter.setTargetRPS(3.5);
-          intake.setPower(0.9);
-        }else if(i < 100){//flip out
-          shooter.setPower(0);
-          intake.setPower(0);
-          dumper.setPower(0.15);
-        }else if(i < 120){//wait
-          dumper.setPower(0);
-        }else if(i < 150){//flip back
-          dumper.setPower(-0.50);
-        }else{ //reset
-          shooter.setPower(0);
-          intake.setPower(0);
-          dumper.setPower(0);
-        }
-      } */
+    /*
+     * for (int i = 0; i < 152; i++) {
+     * if(i < 70){//bloop
+     * shooter.setTargetRPS(3.5);
+     * intake.setPower(0.9);
+     * }else if(i < 100){//flip out
+     * shooter.setPower(0);
+     * intake.setPower(0);
+     * dumper.setPower(0.15);
+     * }else if(i < 120){//wait
+     * dumper.setPower(0);
+     * }else if(i < 150){//flip back
+     * dumper.setPower(-0.50);
+     * }else{ //reset
+     * shooter.setPower(0);
+     * intake.setPower(0);
+     * dumper.setPower(0);
+     * }
+     * }
+     */
 
-    if(opController.getYButton()){
-      //dumper.close();
+    if (opController.getYButton()) {
+      // dumper.close();
       dumper.setPower(0.15);
-    }else if(opController.getXButton()){
-      //dumper.open();
+    } else if (opController.getXButton()) {
+      // dumper.open();
       dumper.setPower(-0.40);
-    }else{
+    } else {
       dumper.setPower(0);
     }
 
-   // System.out.println("Left: " + dumper.leftServo.getAngle() + " Right: " + dumper.rightServo.getAngle());
-   System.out.println(driveTrain.gyro.getAngle());
+    // System.out.println("Left: " + dumper.leftServo.getAngle() + " Right: " +
+    // dumper.rightServo.getAngle());
+    System.out.println(driveTrain.gyro.getAngle());
+
+   if(intake.hasPiece()){
+    lights.setColor(0, 255, 0);
+   }else if(intake.hasPieceFar()){
+    lights.setColor(255, 255, 0);
+   }else{
+    lights.setColor(255, 0, 0);
+   }
 
   }
 
   @Override
-  public void disabledInit() {dumper.setSpeed();
+  public void disabledInit() {
+    dumper.setSpeed();
   }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
+
+  
+
 }
